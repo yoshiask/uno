ad480c4c0ca2731e8d1dae12762a7dfbd5d84c1a
Maxime Dion <maxime.dion@nventive.com>
2019-07-17 14:25:11 -0400

    #23 Migrate private UI Tests to public Uno AdaptiveTrigger

---
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml/AdaptativeTrigger/UnoSamples_Tests.AdaptiveTrigger.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers;
+using Uno.UITest.Helpers.Queries;
+using Xamarin.UITest;
+
+namespace SamplesApp.UITests.Windows_UI_Xaml.AdaptativeTrigger
+{
+	[TestFixture]
+	partial class UnoSamples_Tests : SampleControlUITestBase
+	{
+		[Test]
+		public void AdaptiveTrigger_TextBlock_Validation()
+		{
+			Run("UITests.Shared.AdaptiveTrigger.AdaptiveTrigger_TextBlock");
+
+			var textblock = _app.Marked("AdaptiveTriggerTextBlock");
+
+			//Verify in Portrait
+			_app.SetOrientationPortrait();
+			_app.Wait(2);
+			Assert.AreEqual("Narrow Text", textblock.GetDependencyPropertyValue("Text")?.ToString());
+			Assert.AreEqual("30", textblock.GetDependencyPropertyValue("FontSize")?.ToString());
+
+			//Verify in Landscape
+			_app.SetOrientationLandscape();
+			_app.Wait(2);
+			Assert.AreEqual("Wide Text", textblock.GetDependencyPropertyValue("Text")?.ToString());
+			Assert.AreEqual("50", textblock.GetDependencyPropertyValue("FontSize")?.ToString());
+
+			//Again verify in Portrait
+			_app.SetOrientationPortrait();
+			_app.Wait(2);
+			Assert.AreEqual("Narrow Text", textblock.GetDependencyPropertyValue("Text")?.ToString());
+			Assert.AreEqual("30", textblock.GetDependencyPropertyValue("FontSize")?.ToString());
+		}
+
+		[Test]
+		public void AdaptiveTrigger_TextBox_Validation()
+		{
+			Run("UITests.Shared.AdaptiveTrigger.AdaptiveTrigger_TextBox");
+
+			var textbox = _app.Marked("AdaptiveTriggerTextBox");
+			var textBlockFontSizeValue = _app.Marked("TextBlockFontSizeValue");
+
+			_app.DismissKeyboard();
+			_app.Wait(1);
+
+			//Verify in Portrait
+			_app.SetOrientationPortrait();
+			_app.Wait(2);
+
+			Assert.AreEqual("Narrow font textbox", textbox.GetDependencyPropertyValue("Text")?.ToString());
+			Assert.AreEqual("20", textBlockFontSizeValue.GetDependencyPropertyValue("Text")?.ToString());
+
+			//Verify in Landscape			
+			_app.SetOrientationLandscape();
+			_app.Wait(2);
+
+			Assert.AreEqual("Wide font textbox", textbox.GetDependencyPropertyValue("Text")?.ToString());
+			Assert.AreEqual("40", textBlockFontSizeValue.GetDependencyPropertyValue("Text")?.ToString());
+
+			//Again verify in Portrait
+			_app.SetOrientationPortrait();
+			_app.Wait(2);
+
+			Assert.AreEqual("Narrow font textbox", textbox.GetDependencyPropertyValue("Text")?.ToString());
+			Assert.AreEqual("20", textBlockFontSizeValue.GetDependencyPropertyValue("Text")?.ToString());
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/AdaptiveTrigger/AdaptiveTrigger_TextBlock.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace UITests.Shared.AdaptiveTrigger
+{
+	[SampleControlInfoAttribute("AdaptiveTrigger", "AdaptiveTrigger_TextBlock")]
+	public sealed partial class AdaptiveTrigger_TextBlock : UserControl
+	{
+		public AdaptiveTrigger_TextBlock()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
+
