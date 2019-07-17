161722e78947ca2bfd2e784211f230c708ea11c3
Maxime Dion <maxime.dion@nventive.com>
2019-07-17 10:42:17 -0400

    #23 Migrate private UI Tests to public Uno DatePicker

---
+++ src/SamplesApp/SamplesApp.UITests/Windows_UI_Xaml_Controls/DatePickerTests/UnoSamples_Tests.DatePicker.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers;
+using Uno.UITest.Helpers.Queries;
+using Xamarin.UITest;
+
+namespace SamplesApp.UITests.Windows_UI_Xaml_Controls.DatePickerTests
+{
+	[TestFixture]
+	partial class UnoSamples_Tests : SampleControlUITestBase
+	{
+		[Test]
+		public void DatePicker_IsEnabled_Validation()
+		{
+			Run("UITests.Shared.Windows_UI_Xaml_Controls.DatePicker.Date_Picker_Automated");
+
+			_app.WaitForElement(_app.Marked("btnApplyNewDate"));
+
+			var txtSelectedDate = _app.Marked("txtSelectedDate");
+			var myDatePicker = _app.Marked("myDatePicker");
+			var toggleMyDatePickerIsEnable = _app.Marked("ToggleMyDatePickerIsEnable");
+			var previousDate = txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString();
+
+			// Assert inital state 
+			Assert.AreEqual("1/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+			Assert.AreEqual("True", myDatePicker.GetDependencyPropertyValue("IsEnabled")?.ToString());
+			_app.Screenshot("DatePicker - IsEnabled Validation - 1 - InitialState");
+
+			// Disable date Picker
+			toggleMyDatePickerIsEnable.Tap();
+
+			// Assert that date Picker is disabled
+			Assert.AreEqual("False", myDatePicker.GetDependencyPropertyValue("IsEnabled")?.ToString());
+
+			// Open DatePicker
+			myDatePicker.Tap();
+
+			// Assert that date Picker is disabled
+			_app.Screenshot("DatePicker - IsEnabled Validation - 2 - After clicking when DatePicker disabled");
+		}
+
+		[Test]
+		public void DatePicker_Month_Validation()
+		{
+			Run("UITests.Shared.Windows_UI_Xaml_Controls.DatePicker.Date_Picker_Automated");
+
+			_app.WaitForElement(_app.Marked("btnApplyNewDate"));
+
+			var txtSelectedDate = _app.Marked("txtSelectedDate");
+			var myDatePicker = _app.Marked("myDatePicker");
+
+			var btnJan = _app.Marked("btnJan");
+			var btnFeb = _app.Marked("btnFeb");
+			var btnMar = _app.Marked("btnMar");
+			var btnApr = _app.Marked("btnApr");
+			var btnMay = _app.Marked("btnMay");
+			var btnJune = _app.Marked("btnJune");
+			var btnJuly = _app.Marked("btnJuly");
+			var btnAug = _app.Marked("btnAug");
+			var btnSep = _app.Marked("btnSep");
+			var btnOct = _app.Marked("btnOct");
+			var btnNov = _app.Marked("btnNov");
+			var btnDec = _app.Marked("btnDec");
+
+			// Assert inital state 
+			Assert.AreEqual("1/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to February
+			btnFeb.Tap();
+			Assert.AreEqual("2/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to March
+			btnMar.Tap();
+			Assert.AreEqual("3/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to April
+			btnApr.Tap();
+			Assert.AreEqual("4/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to May
+			btnMay.Tap();
+			Assert.AreEqual("5/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to June
+			btnJune.Tap();
+			Assert.AreEqual("6/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to July
+			btnJuly.Tap();
+			Assert.AreEqual("7/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to August
+			btnAug.Tap();
+			Assert.AreEqual("8/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to September
+			btnSep.Tap();
+			Assert.AreEqual("9/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to October
+			btnOct.Tap();
+			Assert.AreEqual("10/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to November
+			btnNov.Tap();
+			Assert.AreEqual("11/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to December
+			btnDec.Tap();
+			Assert.AreEqual("12/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Setting month to January
+			btnJan.Tap();
+			Assert.AreEqual("1/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+		}
+
+		[Test]
+		public void DatePicker_Day_Validation()
+		{
+			Run("UITests.Shared.Windows_UI_Xaml_Controls.DatePicker.Date_Picker_Automated");
+
+			_app.WaitForElement(_app.Marked("btnApplyNewDate"));
+
+			var txtSelectedDate = _app.Marked("txtSelectedDate");
+			var txtNewDay = _app.Marked("txtNewDay");
+			var btnApplyNewDate = _app.Marked("btnApplyNewDate");
+
+			// Assert inital state 
+			Assert.AreEqual("1/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Change day
+			txtNewDay.Tap();
+			_app.EnterText("7");
+			_app.DismissKeyboard();
+			btnApplyNewDate.Tap();
+
+			// Assert after changing Day 
+			Assert.AreEqual("1/7/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+		}
+
+		[Test]
+		public void DatePicker_Year_Validation()
+		{
+			Run("UITests.Shared.Windows_UI_Xaml_Controls.DatePicker.Date_Picker_Automated");
+
+			_app.WaitForElement(_app.Marked("btnApplyNewDate"));
+
+			var txtSelectedDate = _app.Marked("txtSelectedDate");
+			var txtNewYear = _app.Marked("txtNewYear");
+			var btnApplyNewDate = _app.Marked("btnApplyNewDate");
+
+			// Assert inital state 
+			Assert.AreEqual("1/1/0001 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+
+			// Change day
+			txtNewYear.Tap();
+			_app.EnterText("2014");
+			_app.DismissKeyboard();
+			btnApplyNewDate.Tap();
+
+			// Assert after changing Day 
+			Assert.AreEqual("1/1/2014 12:00:00 AM +00:00", txtSelectedDate.GetDependencyPropertyValue("Text")?.ToString());
+		}
+	}
+}
---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/DatePicker/Date_Picker_Automated.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using SamplesApp.Windows_UI_Xaml_Controls.Models;
+using Uno.UI.Samples.Controls;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.DatePicker
+{
+
+	[SampleControlInfo("Date Picker", nameof(Date_Picker_Automated))]
+	public sealed partial class Date_Picker_Automated : UserControl
+	{
+
+		public Date_Picker_Automated()
+		{
+			this.InitializeComponent();
+			txtSelectedDate.Text = myDatePicker.Date.ToString();
+		}
+
+		private void changeMonth(object sender, RoutedEventArgs e)
+		{
+			var btn = sender as Windows.UI.Xaml.Controls.Button;
+			var btn_value = btn.Content.ToString();
+
+			myDatePicker.Date = new DateTimeOffset(
+				new DateTime(
+					myDatePicker.Date.Year,
+					int.Parse((string)btn.Tag),
+					myDatePicker.Date.Day),
+				new TimeSpan(0)
+			);
+
+			txtSelectedDate.Text = myDatePicker.Date.ToString();
+		}
+
+		private void applyNewDate(object sender, RoutedEventArgs e)
+		{
+			var newDay = txtNewDay.Text != "" ? int.Parse(txtNewDay.Text) : myDatePicker.Date.Day;
+			var newYear = txtNewYear.Text != "" ? int.Parse(txtNewYear.Text) : myDatePicker.Date.Year;
+
+			myDatePicker.Date = new DateTimeOffset(
+				new DateTime(
+					newYear,
+					myDatePicker.Date.Month,
+					newDay),
+				new TimeSpan(0)
+			);
+
+			txtSelectedDate.Text = myDatePicker.Date.ToString();
+		}
+	}
+}
