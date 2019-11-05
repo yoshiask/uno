a113292e755247bacd779017f8476c758fcac15b
David Oliver <david.oliver@nventive.com>
2019-11-05 10:27:27 -0500

    chore(styles): Parse ResourceDictionary correctly in arbitrary context
    Ensure ResourceDictionary is built correctly when, eg, declared inline in a property of object type. This fixes parse failure in CollectionTests Xaml test.

--- src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlConstants.cs
+++ src/SourceGenerators/Uno.UI.SourceGenerators/XamlGenerator/XamlConstants.cs
@@ -65,6 +65,7 @@ namespace Uno.UI.SourceGenerators.XamlGenerator
 			public const string IFrameworkElement = UnoXamlNamespace + ".IFrameworkElement";
 			public const string UIElement = UnoXamlNamespace + ".UIElement";
 			public const string Style = BaseXamlNamespace + ".Style";
+			public const string ResourceDictionary = BaseXamlNamespace + ".ResourceDictionary";
 			public const string ElementStub = BaseXamlNamespace + ".ElementStub";
 			public const string ContentPresenter = Namespaces.Controls + ".ContentPresenter";
 			public const string Markup = BaseXamlNamespace + ".Markup";
