2f76803e01fe2300750de25bb614c6f2acc67787
Carl de Billy <carl.debilly@nventive.com>
2020-01-07 14:33:14 -0500

    Create a sample for new-lines in textblock

---
+++ src/SamplesApp/UITests.Shared/Windows_UI_Xaml_Controls/TextBlockControl/TextBlock_NewLines.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Uno.UI.Samples.Controls;
+
+namespace UITests.Shared.Windows_UI_Xaml_Controls.TextBlockControl
+{
+    [SampleControlInfo]
+    public sealed partial class TextBlock_NewLines : Page
+    {
+        public TextBlock_NewLines()
+        {
+            this.InitializeComponent();
+            txtAssignedInCodeBehind.Text = string.Format("Line 1{0}Line 2{0}Line 3{0}Line 4", Environment.NewLine);
+        }
+    }
+}
