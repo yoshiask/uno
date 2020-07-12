5ae619c31fabf3deae66b63df8498f789211a421
Jerome Laban <jerome.laban@nventive.com>
2020-07-12 18:08:49 -0400

    tests: Bump chromium version

--- build/run-automated-uitests.sh
+++ build/run-automated-uitests.sh
@@ -4,13 +4,13 @@ IFS=$'\n\t'
 
 cd $BUILD_SOURCESDIRECTORY/build/wasm-uitest-binaries
 
-npm i chromedriver@74.0.0
-npm i puppeteer@1.13.0
+npm i chromedriver@83.0.1
+npm i puppeteer@3.1.0
 mono $BUILD_SOURCESDIRECTORY/build/nuget/NuGet.exe install NUnit.ConsoleRunner -Version 3.10.0
 
 export UNO_UITEST_TARGETURI=http://localhost:8000
 export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/wasm-uitest-binaries/node_modules/chromedriver/lib/chromedriver
-export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/wasm-uitest-binaries/node_modules/puppeteer/.local-chromium/linux-637110/chrome-linux/chrome
+export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/wasm-uitest-binaries/node_modules/puppeteer/.local-chromium/linux-756035/chrome-linux/chrome
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wasm-automated-$DOTNET_WASM_RUNTIME
 export UNO_UITEST_PLATFORM=Browser
 
